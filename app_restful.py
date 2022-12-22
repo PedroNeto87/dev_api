9ee87f8bfdedc804fcf80d8eb2b8bc4a9376a4d3
Pedro Neto <pedrofcn01@hotmail.com>
2022-12-22 15:41:30 -0300

    Exercicio aula 'Desenvolvendo uma REST API com a extensão Flask-RESTful'

--- app_restful.py
+++ app_restful.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from habilidades import Habilidades
+from habilidades import Habilidades, Habilidade
 import json
 
 app = Flask(__name__)
@@ -55,6 +55,7 @@ class ListaDesenvolvedores(Resource):
 api.add_resource(Desenvolvedor, '/dev/<int:id>/')
 api.add_resource(ListaDesenvolvedores, '/dev/')
 api.add_resource(Habilidades, '/habilidades/')
+api.add_resource(Habilidade, '/habilidade/<int:id>/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- habilidades.py
+++ habilidades.py
@@ -1,4 +1,6 @@
+from flask import request
 from flask_restful import Resource
+import json
 
 lista_habilidades = ['Python', 'Java', 'Flask']
 
@@ -6,3 +8,26 @@ lista_habilidades = ['Python', 'Java', 'Flask']
 class Habilidades(Resource):
     def get(self):
         return lista_habilidades
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(lista_habilidades)
+        if dados not in lista_habilidades:
+            lista_habilidades.append(dados)
+        else:
+            return {'mensagem': 'Habilidade já cadastra.'}
+        return lista_habilidades
+
+
+class Habilidade(Resource):
+    def put(self, id):
+        dados = json.loads(request.data)
+        lista_habilidades[id] = dados
+        return dados
+
+    def delete(self, id):
+        try:
+            lista_habilidades.pop(id)
+            return {'status': 'Sucesso', 'mensagem': 'Registro excluído.'}
+        except IndexError:
+            return {'status': 'Erro', 'mensagem': 'Registro informado ID {} não existe.'.format(id)}
